cfad2df482f0e07d346a51e555c1625daf0ed6ff
george.ventel <george.ventel@accesa.eu>
2025-12-04 12:48:46 +0200

    innit commit

---
+++ src/main/java/com/rampup/docimporter/DocimporterApplication.java
@@ -0,0 +1,13 @@
+package com.rampup.docimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocimporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocimporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rampup/docimporter/DocimporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rampup.docimporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocimporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
