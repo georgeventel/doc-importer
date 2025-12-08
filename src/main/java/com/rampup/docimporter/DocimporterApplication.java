80574e86e082bc7b2c3dbc40e95d0c2c603ab983
george.ventel <george.ventel@accesa.eu>
2025-12-08 10:20:16 +0200

    optimised imports and reformatted code

--- src/main/java/com/rampup/docimporter/DocimporterApplication.java
+++ src/main/java/com/rampup/docimporter/DocimporterApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 public class DocimporterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DocimporterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DocimporterApplication.class, args);
+    }
 
 }
--- src/main/java/com/rampup/docimporter/client/DocumentClient.java
+++ src/main/java/com/rampup/docimporter/client/DocumentClient.java
@@ -1,10 +1,8 @@
 package com.rampup.docimporter.client;
 
 import com.rampup.docimporter.dto.Document;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
